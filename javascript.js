a778ef8dcfce084f7e3e145b41b60f31c99cfe9e
kapowski <32078611+kapowski@users.noreply.github.com>
2023-04-20 17:47:24 -0700

    Add final score messsage to playGame Cleanup code

--- javascript.js
+++ javascript.js
@@ -2,13 +2,14 @@
 // Playes round of game
 function playRound(playerSelection, computerSelection) {
 
-    // establishes weak vs strong relationship between R P and S elements using nested dictionary
+    // Establishes weak vs strong relationship between R P and S elements using nested dictionary
     const comparisons = {
         Rock: {weakTo: 'Paper', strongTo: 'Scissors'},
         Paper: {weakTo: 'Scissors', strongTo: 'Rock'},
         Scissors: {weakTo: 'Rock', strongTo: 'Paper'} 
     }
 
+    // Round winner announcement
     if (comparisons[playerSelection].strongTo === computerSelection) {
         const win = "You win! " + playerSelection + " beats " + computerSelection;
         return win
@@ -24,50 +25,62 @@ function playRound(playerSelection, computerSelection) {
 // plays 5 round game
 function playGame() {
     
+    // Sets up scorecard
     let playerPoints = 0
     let computerPoints = 0
     
+    // Loops game 5 times
     for(var i=0;i<5;i++) {
-        // computer options
+        
+        // Initiates inputs for game
+        
+        // Computer options
         const computerChoiceArray = ["Rock", "Paper", "Scissors"] 
 
-        // player input
+        // Player input
         var input = prompt("Rock paper or scissors?")
 
-        // converts first letter of input to upper case
+        // Converts first letter of input to upper case
         const playerSelection = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
 
 
         // Randomly selects rock, paper or scissors for computer
         function getComputerChoice(array) {
         
-            // get random index value
+            // Get random index value
             const randomIndex = Math.floor(Math.random() * array.length);
         
-            // get random item
+            // Get random item
             const item = array[randomIndex];
 
             return item;
         }
+        // Initiates playRound
         const computerSelection = getComputerChoice(computerChoiceArray)
         var round = playRound(playerSelection, computerSelection)
-        // console.log(round)
         
+        // Updates scorecard
         if (round === "You win! " + playerSelection + " beats " + computerSelection) {
             playerPoints += 1;
-            console.log(round);
-            console.log(round + " Player Points: " + playerPoints + " Computer Points: " + computerPoints);   
+            console.log(round + ". " + "Player Points: " + playerPoints + " Computer Points: " + computerPoints);  
         } else if (round === "You lose! " + computerSelection + " beats " + playerSelection) {
             computerPoints += 1
-            console.log(round);
-            console.log(round + " Player Points: " + playerPoints + " Computer Points: " + computerPoints);
+            console.log(round + ". " + " Player Points: " + playerPoints + " Computer Points: " + computerPoints);
         } else {
-            console.log(round + " No score" );
-            console.log(round + " Player Points: " + playerPoints + " Computer Points: " + computerPoints);
+            console.log(round + ". " + " Player Points: " + playerPoints + " Computer Points: " + computerPoints);
         }
+    }
+
+    // Final Score Message
+    if (playerPoints > computerPoints) {
+       return "You win the game!";
+    } else if (playerPoints < computerPoints) {
+        return "You lose the game!";
+    } else {
+        return "Tie Game!"
     }
 }
 
-
 // Gameplay
 console.log(playGame())
+
